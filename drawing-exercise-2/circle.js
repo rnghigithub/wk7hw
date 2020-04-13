7dd3f4e96a1edfb85c31a754ef1d9a464d9adc35
Rebecca Nghi <60793465+rnghigithub@users.noreply.github.com>
2020-04-13 18:23:29 -0400

    Add files via upload

--- drawing-exercise-2/circle.js
+++ drawing-exercise-2/circle.js
@@ -2,11 +2,14 @@ const canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
 ctx.beginPath();
-const centerX = canvas.width / 2; 
-const centerY = canvas.height / 2; 
-const radius = 190; 
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
 
-ctx.arc(centerX, centerY, radius, 0, 2 * Math.Pi);
+const radius = 150;
+ctx.arc(centerX, centerY, radius, 0, Math.PI * 2); 
 
+
+ctx.strokeStyle = 'skyblue';
+ctx.lineWidth= '12'
 
 ctx.stroke();
