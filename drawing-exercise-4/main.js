a17c78c0ca89bd71d6c927d19f8fd517860fd2b4
Rebecca Nghi <60793465+rnghigithub@users.noreply.github.com>
2020-04-15 15:19:25 -0400

    Add files via upload

--- drawing-exercise-4/main.js
+++ drawing-exercise-4/main.js
@@ -1,21 +1,22 @@
-function draw() {}
-const canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+function draw() {
+   const canvas = document.getElementById('canvas');
+   let ctx = canvas.getContext('2d');
 
-if (canvas.getContext) {
-    ctx = canvas.getContext('2d');
- } else {
-    const para = document.querySelector('.unsupported');
-    para.textContent = 'Browser does not support HTML5 Canvas';
- }
+   if (canvas.getContext) {
+      ctx = canvas.getContext('2d');
+   } else {
+      const para = document.querySelector('.unsupported');
+      para.textContent = 'Browser does not support HTML5 Canvas';
+   }
 
- var centerX = canvas.width / 2;
- var centerY = canvas.height / 2;
+   var centerX = canvas.width / 2;
+   var centerY = canvas.height / 2;
 
- ctx.fillStyle= "rgb(255, 101, 2)";
+   ctx.fillStyle= "rgb(255, 101, 2)";
 
- ctx.fillRect(centerX - 50, centerY - 45, 100, 80, 50);
- ctx.clearRect(centerX- 15, centerY - 15, 30, 23, 89);
- ctx.strokeRect(centerX - 135, centerY - 80, 250, 150, 76 );
-
- ctx.fill();
+   ctx.fillRect(centerX - 50, centerY - 45, 100, 80, 50);
+   ctx.clearRect(centerX- 15, centerY - 15, 30, 23, 89);
+   ctx.strokeRect(centerX - 135, centerY - 80, 250, 150, 76 );
+}   
+   
+draw ();
